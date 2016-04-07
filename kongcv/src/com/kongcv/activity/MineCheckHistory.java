cc55ff7d03884d9ad62fc8ff744e9d8fcfee4ce1
cuimenglian <cuimenglian@kongcv.com>
2016-04-07 18:32:20 +0800

    modified

--- kongcv/src/com/kongcv/activity/MineCheckHistory.java
+++ kongcv/src/com/kongcv/activity/MineCheckHistory.java
@@ -25,7 +25,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
